ce98daf02a52e98c8acff00fe6db90007a48c2b3
dev.clarova <dev.clarova@gmail.com>
2025-08-25 12:20:00 +0900

    [docs] useState의 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-type SampleProps = {
-  children?: React.ReactNode;
-  age: number;
-  nickName: string;
-};
-const Sample = ({ children, age, nickName }: SampleProps) => {
-  return (
-    <div>
-      {age}살이고요. 별명이 {nickName}인 샘플입니다.
-    </div>
-  );
-};
+import Counter from './components/Counter';
+import NameEditor from './components/NameEditor';
 
-const App = () => {
+function App() {
   return (
     <div>
       <h1>App</h1>
-      <Sample age={20} nickName="홍길동" />
+      <Counter />
+      <NameEditor />
     </div>
   );
-};
+}
 
 export default App;
